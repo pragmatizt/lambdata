060453e266acf427eb0b50edf0417d0b1ccc30bc
pragmatizt <pragmatizt@users.noreply.github.com>
2019-10-01 16:20:53 -0400

    comment 2

---
+++ pragmatizt_functions/__init__.py
@@ -0,0 +1,10 @@
+username@users.noreply.github.com
+"""
+lambdata - a collection of data science helper functions for lambda school
+"""
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES= pd.DataFrame(np.ones(10))
+ZEROS= pd.DataFrame(np.zeros(50))
